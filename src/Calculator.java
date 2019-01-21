3b7931a1716305369f21187c70f9718dc57708cc
ramazan <ramazanocam@gmail.com>
2019-01-21 15:26:18 +0100

    conflict fixed one more time

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,6 @@
 public class Calculator {
-<<<<<<< HEAD
     //this a very useful method.
-=======
-    //add method.
->>>>>>> 267c884cf352e7b55b8db8815b35834313f2fb62
+
     public static double add(double num1, double num2){
         return num1 + num2;
     }
